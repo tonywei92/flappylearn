4bb26a50e4cab8a27c3fbbb5bd5ffd4792e97153
tonywei92 <tonywei92@@gmail.com>
2019-06-05 20:07:30 +0700

    first commit

---
+++ android/src/com/livewithcode/flappylearn/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.livewithcode.flappylearn;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.livewithcode.flappylearn.FlappyLearn;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyLearn(), config);
+	}
+}
